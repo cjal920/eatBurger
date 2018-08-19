8a702ff8315791d5ad604191ae6d7cc4aaf963cd
Colin Larkin <colinlarkin@Colins-MacBook-Air.local>
2018-08-18 22:18:07 -0400

    commit 100

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# eatBurger
+# EAT-A-BURGER
+The dream app for burger lovers. Not only do we offer everything from your fastfood favorites like the Big Mac and the Whopper to gourmet burgers from Rouge and Rembrandts, but we also let you make your own.
+
+And all we need are these npm packages to make the dream come true:
+
+    Express
+    Express-Handlebars
+    Body-Parser
+    Method-Override
+    
+Bon Appetite!
---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Rouge", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Rembrandt", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Double R Bar", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Big Mac", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Whopper", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Five Guys", false);
